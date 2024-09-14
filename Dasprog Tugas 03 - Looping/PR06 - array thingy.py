02c57b3dab736b1b4ac71d1134397a472e06a6b6
FianGumilar <fiangumilaer@gmmail.com>
2024-09-14 09:36:07 +0700

    PR06 update

--- Dasprog Tugas 03 - Looping/PR06 - array thingy.py
+++ Dasprog Tugas 03 - Looping/PR06 - array thingy.py
@@ -6,6 +6,7 @@ array2 = list(map(int, input().split()))
 array1 = array1[:n]
 array2 = array2[:n]
 
+tof = []
 
 m = int(input())
 for i in range(m):
@@ -13,6 +14,12 @@ for i in range(m):
     x = sum(array1[a:b+1])
     y = sum(array2[a:b+1])
     if x == y:
+        tof.append(1)
+    else:
+        tof.append(0)
+
+for i in range(len(tof)):
+    if tof[i] == 1:
         print("YA")
     else:
         print("TIDAK")
