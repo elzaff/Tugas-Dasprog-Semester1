6c7659f6e9ba8bb4771d9f12c3e52bb80d5ab884
elzaff <endercreeper7590@gmail.com>
2024-09-19 17:04:28 +0700

    update king lingkaran

--- Dasprog Praktikum/lingkarangpusing.py
+++ Dasprog Praktikum/lingkarangpusing.py
@@ -2,9 +2,9 @@ x1, y1 = map(int, input().split())
 xs, ys = map(int, input().split())
 xf, yf = map(int, input().split()) 
 
-jarak_king = ((xs - xf)**2 + (ys - yf)**2)**0.5 
+jarak_king = (xs - xf)**2 + (ys - yf)**2
 
-jarak_lingkaran_ke_akhir = ((x1 - xf)**2 + (y1 - yf)**2)**0.5 
+jarak_lingkaran_ke_akhir = (x1 - xf)**2 + (y1 - yf)**2
 
 if int(jarak_lingkaran_ke_akhir) > int(jarak_king):
     print("Yes")
