507f3580d04fd94390d5f35a71a32b7f32be49e2
elzaff <endercreeper7590@gmail.com>
2024-10-02 14:29:44 +0700

    update tumbasbuah

--- Dasprog Tugas 04 - List/tumbasbuah.py
+++ Dasprog Tugas 04 - List/tumbasbuah.py
@@ -7,8 +7,10 @@ for i in range(n):
         jenis.append(i + 1)
 
 bisa = len(jenis)
-if bisa > 0:
+if bisa > 1:
     buah_jenis = ', '.join([f'jenis ke-{j}' for j in jenis[:-1]]) + f', dan jenis ke-{jenis[-1]}'
     print(f"Dengan uang {k} ia bisa membeli dengan kemungkinan {bisa} jenis buah yaitu buah {buah_jenis}")
-else:
+elif bisa == 1:
+    print(f"Dengan uang {k} ia bisa membeli dengan kemungkinan {bisa} jenis buah yaitu buah jenis ke-{jenis[0]}")
+else:    
     print("uange ndak cukup, mulih sek")
